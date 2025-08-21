0d0aac02be6aab6bed26af7cbf577944c7327846
Corey Tibus <coreytibus@gmail.com>
2025-08-20 22:31:57 -0400

    Finished Workshop

---
+++ index.js
@@ -0,0 +1,21 @@
+ console.log("code1")
+
+ console.log("code1", 7 + 3)
+
+ console.log("code2", 100 / 2 - 10)
+
+ console.log("code3", 17 % 5 + 37 )
+
+let message = "The vault has been secured. The combination is:";
+
+let code1 = 10;
+let code2 = 40;
+let code3 = 39;
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
